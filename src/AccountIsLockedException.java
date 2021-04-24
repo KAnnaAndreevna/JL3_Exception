0be8e500a2ae7f810a8d25a40be722e7fd099b51
17802309 <kanna_andreevna@mail.ru>
2021-04-24 21:34:03 +0300

    23/04/2021

--- src/AccountIsLockedException.java
+++ src/AccountIsLockedException.java
@@ -2,9 +2,8 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class AccountIsLockedException extends Exception{
-    public AccountIsLockedException (String mess, Calendar date) {
+    public AccountIsLockedException (String mess) {
         super (mess);
-        System.out.println("TIME out " + date);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,12 @@ import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static Calendar timeLock;
     public static Calendar currentTime;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 1 Проверка на каждое нажатие? - С помощью форм + событий?
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -20,7 +17,7 @@ public class Main {
             String password;
             password = "7777";
             timeLock = new GregorianCalendar();
-            for(int i = 0 ; i < 30 ; i++){
+            for(int i = 0 ; i < 5 ; i++){
 
                 if (!cardLock) {
                     System.out.print("Please enter Your pin-code: ");
@@ -32,6 +29,8 @@ public class Main {
                             cardLock = true;
                             System.out.println("3 failed attempts. Card is locked for 10 second");
                             timeLock = new GregorianCalendar();
+                            Random r = new Random();
+                            Thread.sleep(r.nextInt(10) * 1000);
 
                         } else {
                             System.out.println("Wrong Password");
@@ -43,20 +42,37 @@ public class Main {
                     int hour = currentTime.get(Calendar.HOUR) - timeLock.get(Calendar.HOUR);
                     int min = currentTime.get(Calendar.MINUTE) - timeLock.get(Calendar.MINUTE);
                     int sec = currentTime.get(Calendar.SECOND) - timeLock.get(Calendar.SECOND);
-                    if (hour == 0 && min == 0 && sec < 100) {
-                        System.out.println("Time out!");
-                        throw new AccountIsLockedException("Err", timeLock);
+                    if (hour == 0 && min == 0 && sec < 10) {
+                        //System.out.println("Time out!");
+                        throw new AccountIsLockedException("Time out - " + sec + " second");
                     }
                     else {
                         System.out.println("Card unlock");
                         cardLock = false;
+                        i = 0;
                         continue;
                     }
                 }
 
                 }
-        } catch (IOException | AccountIsLockedException e) {
+        } catch (IOException | AccountIsLockedException | InterruptedException e) {
             e.printStackTrace();
+        }
+
+        //Логика по выдаче наличных - вынести в отдельный класс
+        boolean withdrawalСash = false;
+        while (!withdrawalСash){
+            System.out.print("Please enter amount : ");
+            if (Integer.parseInt(reader.readLine()) % 100 == 0) {
+                System.out.println("The amount is a multiple of 100");
+                //Запросить остаток в банкомате
+
+                // В зависимости от остатка делать withdrawalСash
+                withdrawalСash = true;
+            } else {
+                System.out.println("The amount is not a multiple of 100");
+
+            }
         }
     }
 }
